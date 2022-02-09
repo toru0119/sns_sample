820cfdcfaece535357b26ccd221ba1ae0c5fc633
toru0119 <s.toru.1513@gmail.com>
2022-02-09 11:49:56 +0000

    View Create

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
+  resources :posts, only: [:index, :new, :show, :create, :destroy]
+  resources :users, only: [:show, :edit, :update]
 end
