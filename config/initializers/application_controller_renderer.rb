9e58301f19bdda5938c35114fdda8da57ff558f8
toru0119 <s.toru.1513@gmail.com>
2022-02-10 11:03:46 +0000

    ImageColumn Change NewPost Create

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'e0f75c0019144caa40cac869f2b8780ef5acc2e1950c96e91118bd165941ca840c811d40a5e74061b1d2def3b467f56ea6dbe7362e2c29052632571d9da9e900'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_07_143252) do
+ActiveRecord::Schema.define(version: 2022_02_10_102926) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 2022_02_07_143252) do
 
   create_table "posts", force: :cascade do |t|
     t.integer "user_id"
-    t.string "post_image"
+    t.string "image_id"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2022_02_07_143252) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
