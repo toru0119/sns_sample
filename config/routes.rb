f21aedcd45f3c16747d3c95bc6c59fda1199dff0
toru0119 <s.toru.1513@gmail.com>
2022-02-12 14:45:12 +0000

    Comment Create

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
-  resources :posts, only: [:index, :new, :show, :create, :destroy]
+  resources :posts, only: [:index, :new, :show, :create, :destroy] do
+    resources :comments, only: [:create, :destroy]
+  end
   resources :users, only: [:show, :edit, :update]
 end
