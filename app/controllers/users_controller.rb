9d3691f7086f30a7b6d957394e2e43d7a1c1bbfa
toru0119 <s.toru.1513@gmail.com>
2022-02-16 13:24:31 +0000

    UserProfile Create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :image)
+    params.require(:user).permit(:name, :profile_image)
   end
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,8 @@ class Post < ApplicationRecord
   has_many :likes
 
   attachment :image
+  
+  validates :image, presence: true
 
   def liked_by?(user)
     likes.where(user_id: user.id).exists?
