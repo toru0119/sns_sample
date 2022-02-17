5354e9e8f598d308bdd24e40377cdbf8a77dc705
toru0119 <s.toru.1513@gmail.com>
2022-02-17 14:58:36 +0000

    Relationship Create

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
+  
   root to: 'homes#top'
   resources :posts, only: [:index, :new, :show, :create, :destroy] do
     resource :likes, only: [:create, :destroy]
